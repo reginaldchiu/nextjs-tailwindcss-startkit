7120346a8dff8f7e130ba08320a72ad2cf6dbcf5
邱磊 <qiulei@qiuleideMacBook-Pro.local>
2020-01-19 09:57:32 +0800

    Initial commit

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+const Footer = () => (
+  <footer className="bg-blue-500">
+    <ul className="flex items-center justify-between max-w-4xl mx-auto p-4 md:p-8 text-sm text-white">
+      <li>
+        Created by{" "}
+        <a href="https://code-amaze.net" target="_blank" className="font-bold">
+          Code Amaze
+        </a>
+      </li>
+      <li>Power by Next.js Tailwind CSS</li>
+    </ul>
+  </footer>
+);
+
+export default Footer;
---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout";
+
+const Index = () => (
+  <Layout>
+    <div className="flex flex-col items-center justify-center">
+      <h2 className="bg-yellow-400 font-bold my-8 p-3 text-lg md:text-2xl">
+        Hi! Welcome to your first Next.js site.
+      </h2>
+    </div>
+  </Layout>
+);
+
+export default Index;
